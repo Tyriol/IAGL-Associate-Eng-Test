e9cb94570eff82f5d311bace497276f8d687a643
Ryan <ryanshaunsmith@gmail.com>
2024-11-17 14:49:07 +0000

    add a test for adding a todo

--- backend/test/repository/todo.test.js
+++ backend/test/repository/todo.test.js
@@ -1,14 +1,32 @@
+const repository = require("../../src/repository/todo");
 
-const repository = require('../../src/repository/todo');
-
-describe('TODO repository', () => {
-  it('should return the todo list', async () => {
+describe("TODO repository", () => {
+  it("should return the todo list", async () => {
     const expected = {
-      todos: [{
-        "task": "This is a todo example"
-      }]
+      todos: [
+        {
+          task: "This is a todo example",
+        },
+      ],
     };
     const actual = await repository.getTodos();
+    expect(actual).toEqual(expected);
+  });
+  it("should add a todo to the list", async () => {
+    const newTodo = {
+      task: "New todo item",
+    };
+    const expected = {
+      todos: [
+        {
+          task: "This is a todo example",
+        },
+        {
+          task: "New todo item",
+        },
+      ],
+    };
+    const actual = await repository.addTodo(newTodo);
     expect(actual).toEqual(expected);
   });
 });
--- plan.md
+++ plan.md
@@ -9,6 +9,7 @@ so I can track the things I want to do.
 ## Plan
 
 - Understand what is currently happening in the code
+- Add a test for adding a todo
 - Add a post route to allow a todo to be added to the todo.js repository
 - Add a form on the front-end to allow a user to input their todo item
 - Link front-end form to back end to allow a new todo to be added
