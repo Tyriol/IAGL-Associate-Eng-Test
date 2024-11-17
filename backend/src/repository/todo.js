8c92cd64c0171fce6760b184721372a44e5d24b4
Ryan <ryanshaunsmith@gmail.com>
2024-11-17 15:25:01 +0000

    add route to add new todo in memory

--- backend/src/repository/todo.js
+++ backend/src/repository/todo.js
@@ -1,12 +1,15 @@
-
 let todoList = {
   todos: [
     {
-      "task": "This is a todo example"
-    }
-  ]
+      task: "This is a todo example",
+    },
+  ],
 };
 
 module.exports = {
-  getTodos: () => Promise.resolve(todoList)
+  getTodos: () => Promise.resolve(todoList),
+  addTodo: (todo) => {
+    todoList.todos.push(todo);
+    return Promise.resolve(todoList);
+  },
 };
